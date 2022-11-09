850fbbfe3971b7864d39bb1e462745d37df6be25
Isaac Sexe <isaac.sexe@gmail.com>
2022-11-09 03:38:24 -0600

    Started messing with Listeners/Visitors, need to evaluate terminals somehow

--- Project.py
+++ Project.py
@@ -3,28 +3,44 @@ from antlr4 import *
 from Grammer.ProjectLexer import ProjectLexer
 from Grammer.ProjectParser import ProjectParser
 from Grammer.ProjectListener import ProjectListener
+from Grammer.ProjectVisitor import ProjectVisitor
 
-class HelloPrintListener(ProjectListener):
-    def enterHi(self, ctx):
-        print("Hello: %s" % ctx.ID())
+# *Resource for understanding Listener VS Visitor
+# https://tomassetti.me/listeners-and-visitors/
+class GrammarListener(ProjectListener):
+    def enterExpression(self, ctx: ProjectParser.ExpressionContext):
+        print("Enter Expression: " + ctx.getText())
+
+    def exitExpression(self, ctx: ProjectParser.ExpressionContext):
+        print("Exit Expression: " + ctx.getText())
+
+    def visitTerminal(self, node: TerminalNode):
+        print("Visit Terminal: " + str(node))
+
+class GrammarVisitor(ProjectVisitor):
+    def visitExpression(self, ctx: ProjectParser.ExpressionContext):
+        print("Visit Expression: " + ctx.getText())
 
 def main():
     # command-line formatting
     print("pȳthon 0.0.1")
     print("Type Ctrl+D to run, Ctrl+Z to exit")
-    usr_input = StdinStream(input('>>> '))
-
+    usr_input = StdinStream('utf-8')
+    
     # actual useful stuff
     lexer = ProjectLexer(usr_input)
     stream = CommonTokenStream(lexer)
     parser = ProjectParser(stream)
+    parser.buildParseTrees = True
     
-    tree = parser.hi()
-    
-    printer = HelloPrintListener()
-    walker = ParseTreeWalker()
+    tree = parser.expression()
+    listener = GrammarListener()
 
-    walker.walk(printer, tree)
+    walker = ParseTreeWalker()
+    walker.walk(listener, tree)
+
+    print(tree.toStringTree())
+    print(tree.getText())
 
 if __name__ == '__main__':
     main()
